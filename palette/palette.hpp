64d7939c4459e15b738983c1872a6d3644bee49c
Ahmed <malik.aziz.cloud@gmail.com>
2025-12-04 00:00:36 +0200

    Add palette header with color definitions

---
+++ palette/palette.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include "ftxui/screen/color.hpp"
+
+namespace Pal {
+    constexpr auto black      = ftxui::Color::RGB(0,0,0);
+    constexpr auto green      = ftxui::Color::RGB(0,255,0);
+    constexpr auto white      = ftxui::Color::RGB(240,240,240);
+    constexpr auto dim_amber  = ftxui::Color::RGB(180,120,0);
+}
