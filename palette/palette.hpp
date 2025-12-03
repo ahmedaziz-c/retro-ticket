39ff885d97a0c94ad5ff7f6ed0cd95cb84531c99
Ahmed <malik.aziz.cloud@gmail.com>
2025-12-04 00:13:15 +0200

    Delete palette directory

--- palette/palette.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "ftxui/screen/color.hpp"
-
-namespace Pal {
-    constexpr auto black      = ftxui::Color::RGB(0,0,0);
-    constexpr auto green      = ftxui::Color::RGB(0,255,0);
-    constexpr auto white      = ftxui::Color::RGB(240,240,240);
-    constexpr auto dim_amber  = ftxui::Color::RGB(180,120,0);
-}
