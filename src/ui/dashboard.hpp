7931191dc15a09a8e61c41ef295eee8bad9fbab2
Ahmed <malik.aziz.cloud@gmail.com>
2025-12-04 00:01:57 +0200

    Add Dashboard class with basic structure

---
+++ src/ui/dashboard.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include "ftxui/screen/screen.hpp"
+#include "ftxui/component/component.hpp"
+
+class Dashboard {
+public:
+    Dashboard(ftxui::ScreenInteractive& s);
+    ftxui::Component renderer();
+private:
+    ftxui::Component container_;
+};
